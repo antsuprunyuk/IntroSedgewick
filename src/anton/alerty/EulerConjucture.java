1e39a8c14b4cf7714d3e77124705f36717994db8
Anton Suprunyuk <antsuprunyuk@gmail.com>
2014-10-28 17:22:49 +0200

    281014

--- src/anton/alerty/EulerConjucture.java
+++ src/anton/alerty/EulerConjucture.java
@@ -4,35 +4,41 @@ public class EulerConjucture {
 
 	public static void main(String[] args) {
 		long N = Long.parseLong(args[0]);
-		//long a5, b5, c5, d5, e5;
-metka:	for (long e = 1; e*e*e*e*e <= N; e++) {
-			//if (e % 10 == 0) System.out.println(e);
+		long a5= 0, b5 = 0, c5 = 0, d5 = 0, e5 = 0;
+metka:	for (long e = 1; e5 <= N; e++) {
+			if (e % 1000 == 0) {System.out.println(e);}
 			long sum = 0;
-			long e5 = e*e*e*e*e;
-			for (long a = 1 ; a*a*a*a*a < e5; a++) {
-				long a5 = a*a*a*a*a;
+			e5 = e*e*e*e*e;
+			for (long a = 1 ; sum < e5; a++) {
+				a5 = a*a*a*a*a;
 				sum = a5;
 				if (sum > e5) break;
-				for (long b = a + 1; b*b*b*b*b + sum < e5; b++) {
-					long b5 = b*b*b*b*b;
+				for (long b = a + 1;  sum < e5; b++) {
+					b5 = b*b*b*b*b;
 					sum = b5 + a5;
 					if (sum > e5) break;
-					for (long c = b + 1; c*c*c*c*c + sum  < e5; c++) {
-						long c5 = c*c*c*c*c;
+					for (long c = b + 1; sum  < e5; c++) {
+						c5 = c*c*c*c*c;
 						sum = a5 + b5 + c5;
 						if (sum > e5) break;
-						for (long d = c + 1; d*d*d*d*d + sum < e5; d++) {
-							long d5 = d*d*d*d*d;
+						for (long d = c + 1; sum <= e5; d++) {
+							d5 = d*d*d*d*d;
 							sum = a5 + b5 + c5 + d5;
-							if (sum > e5) break;
+							//System.out.println(e + " = " + a + " " + b + " " + c + " " + d);
+							//if (sum > e5) break;
 							if (e5 == sum) {
 								System.out.println(e + " = " + a + " " + b + " " + c + " " + d);
-								break metka;
+								System.out.println(e5 + " = " + a5 + " " + b5 + " " + c5 + " " + d5);
+								//break metka;
 							}
 						}
+						sum -= d5;	
 					}
+					sum -= c5;	
 				}
+				sum -= b5;	
 			}
+			sum -= a5;	
 		}
 	}
 }
